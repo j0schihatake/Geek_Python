823eb92ff5e6cc066e0dfad3257812b46de9fe3d
j0schiTadamori <j0schitadamori@gmail.com>
2021-01-31 10:54:56 +0300

    docker и первая функция.

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,10 @@ RUN mkdir -p /usr/src/app/
 WORKDIR /usr/src/app/
 
 copy . /usr/src/app/
+RUN pip install --no-cache-dir -r requirements.txt
+
+EXPOSE 8080
+
+ENV TZ Europe/Moscow
 
 CMD ["python", "docker_lesson.py"]
--- docker_lesson.py
+++ docker_lesson.py
@@ -7,12 +7,35 @@ from src.lesson_1 import *
 # docker build -t hello-world .
 
 # Docker, просмотр образов
-# docker images
 
-# Docker, запуск с последующим удалением образа
-# docker run --rm hello-world
+# docker images
+# docker ps -a
+# docker ps -a -q (айдишники)
+
+# Docker, запуск
+# docker run --rm --name test_img hello-world
+# docker run --rm --name test_img -p 8080:8080 hello-world
+# docker run --rm --name test_img -p 8080:8080 -e TZ=Europe/Moscow hello-world
+# docker run --rm --name test_img -p 8080:8080 -e TZ=Europe/Moscow -v hello:/usr/src/app/src/ hello-world
+# docker run --rm -d --name test_img -p 8080:8080 -e TZ=Europe/Moscow -v hello:/usr/src/app/src/ hello-world
+# docker run --name test_img hello-world
+
+# Docker, удаление образа
+# docker rm "чтото из параметров"
+# docker rm $(docker ps -qa)
+# docker rm -vf $(docker ps -a -q)
+# docker rmi -f $(docker images -a -q)
+# docker rm -f SOMENAME
+# $images = docker images -a -q
+# foreach ($image in $images) { docker image rm $image -f }
+
+# Остановка
+# docker stop $(docker ps -q) -a
+
+# pip
+# pip install -r requirements.txt
+
+# volume
+# docker volume ls
 
 print(docker_help())
-
-
-
